3461b73fcd25345def009b1db676f100f13037b1
Rednata <maknatosh@gmail.com>
2023-01-24 23:26:32 +0700

    feat: change prices.css for acordion

---
+++ plants/scrypt/modules/prices.js
@@ -0,0 +1,5 @@
+const showPrice = () => {
+  
+};
+
+export {showPrice};
--- plants/scrypt/script.js
+++ plants/scrypt/script.js
@@ -1,9 +1,11 @@
 import { menuControl } from './modules/menuControl.js';
+import { showPrice } from './modules/prices.js';
 import { onClickServiceBtn } from './modules/service-card.js';
 
 const init = () => {
   menuControl();
   onClickServiceBtn();
+  showPrice();
 };
 
 init();
