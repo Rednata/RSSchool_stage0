bacfe34a6540b340d554a9d338a36087fa4781db
Rednata <maknatosh@gmail.com>
2023-01-25 00:13:00 +0700

    feat: make acordion for prices.js

--- plants/scrypt/modules/prices.js
+++ plants/scrypt/modules/prices.js
@@ -1,5 +1,21 @@
 const showPrice = () => {
-  
-};
+  const priceBtns = document.querySelectorAll('.category');
+
+  priceBtns.forEach(btn => {
+    btn.addEventListener('click', ({target}) => {          
+      if (!target.closest('.category__order')) {
+        const currentBtn = btn;
+        btn.classList.toggle('category_active');
+        priceBtns.forEach(btn => {
+          if (btn !== currentBtn) {
+            btn.classList.remove('category_active')
+          }          
+        })
+      }
+      
+    })
+
+  })
+}
 
 export {showPrice};
