472803c6de6e8ff15dee8f4fb7b6543d526147d7
Rednata <maknatosh@gmail.com>
2023-02-13 12:20:15 +0700

    feat: create playList

---
+++ momentum/script/modules/playlist.js
@@ -0,0 +1,21 @@
+export const playList = [
+  {      
+    title: 'Aqua Caelestis',
+    src: '../assets/sounds/AquaCaelestis.mp3',    
+  },  
+  {      
+    title: 'Ennio Morricone',
+    src: '../assets/sounds/EnnioMorricone.mp3',    
+  },
+  {      
+    title: 'River Flows In You',
+    src: '../assets/sounds/RiverFlowsInYou.mp3',    
+  },
+  {      
+    title: 'Summer Wind',
+    src: '../assets/sounds/SummerWind.mp3',    
+  }
+];
+
+
+
